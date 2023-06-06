04a6208ea77a4fb3ae8a8905c72ff876501c5667
Benjamin Rogers <BenjaminRogersSeng>
2023-06-06 12:07:16 +0000

    Initial commit

---
+++ src/test/java/com/example/SENG4400_A2Test.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class SENG4400_A2Test {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
